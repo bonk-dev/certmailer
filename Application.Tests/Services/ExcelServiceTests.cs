a989452108b846b56fa3a6a863b35db9acd5a001
bonk-dev <github.p0j6t@passinbox.com>
2025-07-15 13:44:04 +0200

    style: remove unnecessary arg names in test data

--- Application.Tests/Services/ExcelServiceTests.cs
+++ Application.Tests/Services/ExcelServiceTests.cs
@@ -10,40 +10,40 @@ public class ExcelServiceTests
     public async Task TestParseValid()
     {
         var testFilePath = Path.Combine(Directory.GetCurrentDirectory(), "TestFiles", "participants_valid.xlsx");
-        Debug.WriteLine("TestParseValid: {0}", args: testFilePath);
+        Debug.WriteLine("TestParseValid: {0}", testFilePath);
 
         await using var stream = File.OpenRead(testFilePath);
         var service = Testing.GetRequiredService<IExcelService>();
-        var result = await service.ParseAsync(stream, sheetName: "Participants");
+        var result = await service.ParseAsync(stream, "Participants");
         
         Assert.Multiple(() =>
         {
-            Assert.IsTrue(result.Success, message: "Parsing was not successful");
-            Assert.IsEmpty(result.Errors, message: "Parsing ended with errors: " + string.Join(';', result.Errors));
+            Assert.IsTrue(result.Success, "Parsing was not successful");
+            Assert.IsEmpty(result.Errors, "Parsing ended with errors: " + string.Join(';', result.Errors));
         });
         
         var expected = new Participant[]
         {
-            new(FirstName: "Aleksander", LastName: "Pietrzak", Email: "aleksander.pietrzak@poczta.fm",
-                CourseName: "Programowanie Java", CompletionDate: new DateTime(2024, 10, 8)),
-            new(FirstName: "Robert", LastName: "Wiśniewski", Email: "robert518@gmail.com",
-                CourseName: "Administracja Biurowa", CompletionDate: new DateTime(2025, 4, 4)),
-            new(FirstName: "Joanna", LastName: "Jabłoński", Email: "joannajablonski@poczta.fm",
-                CourseName: "Prezentacje Multimedialne", CompletionDate: new DateTime(2025, 1, 21)),
-            new(FirstName: "Dorota", LastName: "Wojciechowski", Email: "dorota.wojciechowski@onet.pl",
-                CourseName: "Logistyka i Transport", CompletionDate: new DateTime(2025, 1, 19)),
-            new(FirstName: "Aleksandra", LastName: "Tomaszewski", Email: "aleksandra81@onet.pl",
-                CourseName: "Język Hiszpański - Konwersacje", CompletionDate: new DateTime(2024, 8, 27)),
-            new(FirstName: "Renata", LastName: "Jankowski", Email: "r.jankowski@gmail.com",
-                CourseName: "Logistyka i Transport", CompletionDate: new DateTime(2025, 3, 17)),
-            new(FirstName: "Paweł", LastName: "Krawczyk", Email: "p.krawczyk@onet.pl",
-                CourseName: "Język Angielski - Poziom B1", CompletionDate: new DateTime(2024, 8, 5)),
-            new(FirstName: "Weronika", LastName: "Baran", Email: "weronika176@poczta.fm", CourseName: "Prawo Pracy",
-                CompletionDate: new DateTime(2025, 4, 22)),
-            new(FirstName: "Małgorzata", LastName: "Krawczyk", Email: "m.krawczyk@gmail.com", CourseName: "Prawo Pracy",
-                CompletionDate: new DateTime(2025, 6, 28)),
-            new(FirstName: "Małgorzata", LastName: "Krawczyk", Email: "malgorzatakrawczyk@poczta.fm",
-                CourseName: "Informatyka dla Seniorów", CompletionDate: new DateTime(2024, 12, 24))
+            new("Aleksander", "Pietrzak", "aleksander.pietrzak@poczta.fm",
+                "Programowanie Java", new DateTime(2024, 10, 8)),
+            new("Robert", "Wiśniewski", "robert518@gmail.com",
+                "Administracja Biurowa", new DateTime(2025, 4, 4)),
+            new("Joanna", "Jabłoński", "joannajablonski@poczta.fm",
+                "Prezentacje Multimedialne", new DateTime(2025, 1, 21)),
+            new("Dorota", "Wojciechowski", "dorota.wojciechowski@onet.pl",
+                "Logistyka i Transport", new DateTime(2025, 1, 19)),
+            new("Aleksandra", "Tomaszewski", "aleksandra81@onet.pl",
+                "Język Hiszpański - Konwersacje", new DateTime(2024, 8, 27)),
+            new("Renata", "Jankowski", "r.jankowski@gmail.com",
+                "Logistyka i Transport", new DateTime(2025, 3, 17)),
+            new("Paweł", "Krawczyk", "p.krawczyk@onet.pl",
+                "Język Angielski - Poziom B1", new DateTime(2024, 8, 5)),
+            new("Weronika", "Baran", "weronika176@poczta.fm", "Prawo Pracy",
+                new DateTime(2025, 4, 22)),
+            new("Małgorzata", "Krawczyk", "m.krawczyk@gmail.com", "Prawo Pracy",
+                new DateTime(2025, 6, 28)),
+            new("Małgorzata", "Krawczyk", "malgorzatakrawczyk@poczta.fm",
+                "Informatyka dla Seniorów", new DateTime(2024, 12, 24))
         };
         
         Assert.That(result.Data, Is.EquivalentTo(expected));
