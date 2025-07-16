dbfc42a5e0504143715df992a60bf3b6078abb8b
bonk-dev <github.p0j6t@passinbox.com>
2025-07-16 19:30:13 +0200

    feat: remove test HTTP endpoint

--- ExcelParser.WebUI/Endpoints/UploadEndpoint.cs
+++ ExcelParser.WebUI/Endpoints/UploadEndpoint.cs
@@ -46,20 +46,4 @@ public class UploadEndpoint : ControllerBase
         var addResult = await _mediator.Send(command);
         return Ok(new JobDto(addResult, "uploaded"));
     }
-
-    [HttpPost("test")]
-    public async Task<IActionResult> OnPostTestAsync(
-        [FromForm] int? mailTemplateId, [FromForm] int? subjectTemplateId)
-    {
-        await using var s = System.IO.File.OpenRead(
-            "/home/bonk/Programowanie/CertMailer-data/participants_valid.xlsx");
-        var command = new AddJobCommand
-        {
-            FileStream = s,
-            MailTemplateId = mailTemplateId,
-            SubjectTemplateId = subjectTemplateId
-        };
-        var addResult = await _mediator.Send(command);
-        return Ok(new JobDto(addResult, "uploaded"));
-    }
 }
