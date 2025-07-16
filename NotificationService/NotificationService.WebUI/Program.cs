c194fe5b475bdcb686d289ddaa06b2f810ff2a20
bonk-dev <github.p0j6t@passinbox.com>
2025-07-16 13:19:36 +0200

    feat: add WebUI project for NotificationService

---
+++ NotificationService/NotificationService.WebUI/Program.cs
@@ -0,0 +1,23 @@
+using CertMailer.NotificationService.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services
+    .AddOpenApi()
+    .AddInfrastructureServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.Run();
